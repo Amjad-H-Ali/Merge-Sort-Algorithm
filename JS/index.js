4fef74bc78d67dcbb3bfa38b88ca7aa52741b5b7
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-16 18:07:19 -0500

    Function finds  right half of the array. Also, stores array length into variable.

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,19 @@
 console.log('Merge Sort');
 
 const mergeSort = (array) => {
+	const arrayLength = array.length; // Store array's length
 	// If the array only contains 1 item, return it. Ignore everything else.
-	if (array.length === 1) {
-
+	if (arrayLength === 1) {
 		return array
 	}
 	// Find the center of the array's index and store it into variable.
-	const middle = Math.floor(array.length/2);
+	const middle = Math.floor(arrayLength/2);
 	// Find the left half of the array and store it into variable.
 	const left = array.slice(0, middle);
+	// Find the right half of the array and store it into variable.
+	const right = array.slice(middle);
 
-	return left;
+	return right;
 
 
 }
