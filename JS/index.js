c23c47ac22cf5f2cc1a9fce44ce3c1aeb58ac892
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-16 17:43:52 -0500

    Function returns array if length is equal to 1

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,15 @@
 console.log('Merge Sort');
+
+const mergeSort = (array) => {
+
+	if (array.length === 1) {
+
+		return array
+	}
+	
+	return "Length is NOT equal to 1"
+	
+
+}
+
+console.log(mergeSort([1]));
