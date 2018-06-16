21c984c1d6a0b1603aa21e08e4453547133402d2
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-06-16 17:57:33 -0500

    Function returns center of the array

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,17 @@
 console.log('Merge Sort');
 
 const mergeSort = (array) => {
-
+	// If the array only contains 1 item, return it. Ignore everything else.
 	if (array.length === 1) {
 
 		return array
 	}
-	
-	return "Length is NOT equal to 1"
-	
+	// Find the center of the array's index and store it into variable.
+	const middle = Math.floor(array.length/2);
+
+	return array[middle];
+
 
 }
 
-console.log(mergeSort([1]));
+console.log(mergeSort([1,2,3,4,5,6,7]));
